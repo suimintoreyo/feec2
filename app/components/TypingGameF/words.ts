ee50d48b0401907c5b7214bdb795d3b05434665d
suimintoreyo <166894717+suimintoreyo@users.noreply.github.com>
2024-09-06 15:00:45 +0900

    word.tsローマ字便意上変更

--- app/components/TypingGameF/words.ts
+++ app/components/TypingGameF/words.ts
@@ -3,7 +3,7 @@
 export const words = [
     { display: "りんご", furigana: "りんご", kanji: "りんご", typing: "ringo" },
     { display: "バナナ", furigana: "ばなな", kanji: "バナナ", typing: "banana" },
-    { display: "さくらんぼ", furigana: "さくらんぼ", kanji: "桜んぼ", typing: "sakuranbo" },
-    { display: "なつめやし", furigana: "なつめやし", kanji: "棗椰子", typing: "natsumeyashi" },
+    { display: "さくらんぼ", furigana: "さくらんぼ", kanji: "桜んぼ", typing: "sakurannbo" },
+    { display: "なつめやし", furigana: "なつめやし", kanji: "棗椰子", typing: "natumeyasi" },
     { display: "エルダーベリー", furigana: "えるだーべりー", kanji: "エルダーベリー", typing: "eruda-beri-" }
 ];
